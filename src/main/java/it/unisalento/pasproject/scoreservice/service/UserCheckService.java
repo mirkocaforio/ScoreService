c480b82bbcaefec26707396005585de0640a425e
mirkocaforio <mirkocaforio2002@gmail.com>
2024-05-20 16:59:23 +0200

    Initial setup

---
+++ src/main/java/it/unisalento/pasproject/scoreservice/service/UserCheckService.java
@@ -0,0 +1,75 @@
+package it.unisalento.pasproject.scoreservice.service;
+
+
+import it.unisalento.pasproject.scoreservice.business.exchanger.MessageExchangeStrategy;
+import it.unisalento.pasproject.scoreservice.business.exchanger.MessageExchanger;
+import it.unisalento.pasproject.scoreservice.dto.UserDetailsDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static it.unisalento.pasproject.scoreservice.security.SecurityConstants.ROLE_ADMIN;
+
+
+@Service
+public class UserCheckService {
+
+    private final MessageExchanger messageExchanger;
+
+    @Value("${rabbitmq.exchange.security.name}")
+    private String securityExchange;
+
+    @Value("${rabbitmq.routing.security.key}")
+    private String securityRequestRoutingKey;
+
+    @Autowired
+    public UserCheckService(MessageExchanger messageExchanger, @Qualifier("RabbitMQExchange") MessageExchangeStrategy messageExchangeStrategy) {
+        this.messageExchanger = messageExchanger;
+        this.messageExchanger.setStrategy(messageExchangeStrategy);
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserCheckService.class);
+
+
+    public UserDetailsDTO loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        //Chiamata MQTT a CQRS per ottenere i dettagli dell'utente
+        UserDetailsDTO user = messageExchanger.exchangeMessage(email,securityRequestRoutingKey,securityExchange,UserDetailsDTO.class);
+
+        if(user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        LOGGER.info(String.format("User %s found with role: %s and enabled %s", user.getEmail(), user.getRole(), user.getEnabled()));
+
+        return user;
+    }
+
+
+    public Boolean isEnable(Boolean enable) {
+        return enable;
+    }
+
+    /**
+     * Check if the current user is the user with the given email
+     * @param email the email of the user to check
+     * @return true if the current user is the user with the given email, false otherwise
+     */
+    public Boolean isCorrectUser(String email){
+        return email.equals(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
+    /**
+     * Check if the current user is an administrator
+     * @return true if the current user is an administrator, false otherwise
+     */
+    public Boolean isAdministrator(){
+        String currentRole = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
+        return currentRole.equalsIgnoreCase(ROLE_ADMIN);
+    }
+}
